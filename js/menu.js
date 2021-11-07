c442f7c286565905afc6473890b5c04b17a20a63
olga2710 <olga.liaz@yandex.ru>
2021-11-07 18:04:28 +0300

    new

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,65 @@
 const restourant = 'food-band';
+const cardsMenu = document.querySelector('.cards-menu');
 
+const changeTitle = (restaurant) =>{
+    const restaurantTitle = document.querySelector('.restaurant-title');
+    const rating = document.querySelector('.rating');
+    const price = document.querySelector('.price');
+    const category = document.querySelector('.category');
 
-const renderItem = (data)=>{
-    let array = [];
-     data.forEach(elem=>{
-        array.push(elem);
-        console.log(elem);
-     });
+    restaurantTitle.textContent = restaurant.name;
+    rating.textContent = restaurant.stars;
+    price.innerHTML = `От ${restaurant.price} ₽`;
+    category.textContent =restaurant.kitchen;
+
 
 }
 
 
-fetch(`https://food-519ce-default-rtdb.firebaseio.com/db/food-band.json`) //получаем данные из файла
+
+const renderItem = (data)=>{
+     data.forEach(({id, name, description, price, image})=>{
+        const card =   document.createElement('div');
+        card.classList.add('card');
+        card.dataset.id = id;
+
+        card.innerHTML = `
+            <img src="${image}" alt="${name}" class="card-image" />
+			<div class="card-text">
+				<div class="card-heading">
+					<h3 class="card-title card-title-reg">${name}</h3>
+				</div>
+				<!-- /.card-heading -->
+				<div class="card-info">
+					<div class="ingredients">
+                    ${description}
+					</div>
+				</div>
+				<!-- /.card-info -->
+				<div class="card-buttons">
+					<button class="button button-primary button-add-cart">
+						<span class="button-card-text">В корзину</span>
+						<span class="button-cart-svg"></span>
+					</button>
+					<strong class="card-price-bold">${price} ₽</strong>
+				</div>
+			</div>
+			<!-- /.card-text -->
+        
+        
+        `;
+
+        cardsMenu.append(card);
+     });
+
+}
+
+if (localStorage.getItem('restaurant')){
+    const restaurant= JSON.parse(localStorage.getItem('restaurant'));
+
+    changeTitle(restaurant);
+
+    fetch(`https://food-519ce-default-rtdb.firebaseio.com/db/${restaurant.products}`) //получаем данные из файла
     .then((response)=>response.json()) //переводим их в формат json 
     .then((data)=>{
         renderItem(data)
@@ -19,3 +67,6 @@ fetch(`https://food-519ce-default-rtdb.firebaseio.com/db/food-band.json`) //по
     .catch((err)=>{ 
         alert('Извините, сервер временно недоступен');  
     })
+
+}
+
